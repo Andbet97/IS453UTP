0835cfc831333d25ef92792286d4bc773c2e464d
Andbet <andbet050197@gmail.com>
2017-02-27 22:35:33 -0500

    Ejercicio 1, dificultad (*)

---
+++ 99_prolog_problems/P01.pl
@@ -0,0 +1,8 @@
+%http://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
+%P01 (*) Find the last element of a list.
+%        Example:
+%        ?- my_last(X,[a,b,c,d]).
+%        X = d
+
+my_last(X, [X]) :- !.
+my_last(X, [_|T]) :- my_last(X, T).
