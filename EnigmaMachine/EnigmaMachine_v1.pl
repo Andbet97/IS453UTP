79e0afc313aa2a2dcb63aa2218645d876566764a
Andbet <andbet050197@gmail.com>
2017-02-28 15:52:52 -0500

    Modificacion 2

--- EnigmaMachine/EnigmaMachine_v1.pl
+++ EnigmaMachine/EnigmaMachine_v1.pl
@@ -3,8 +3,8 @@
 %La distribución es: Reflector B, Rotor III, Rotor II, Rotor I.
 
 %saber la posicion de un elemento en la lista
-%se modificara para saber la lista que tenga como cabeza el elemento
-%la pregunta es -?member(Lista, elemento, P).
+%se modificara para saber la lista que tenga como cabeza el elemento a
+%buscar la pregunta es -?member(Lista, elemento, P).
 member2([[X|_]], X, Acc, Acc).
 member2([[X|_]|_], X, Acc, Acc).
 member2([[H|_]|T], X, Acc, P) :-
@@ -14,4 +14,9 @@ member2([[H|_]|T], X, Acc, P) :-
 member(L, X, P) :- member2(L, X, 1, P).
 
 
-rotor1(X) :- X = [[a, 3], [b, 1], [c, 2], [d, 4]].
+rotor1([[a, 3], [b, 1], [c, 2], [d, 4]]).
+rotor2([[a, 3], [c, 1], [b, 2], [d, 4]]).
+
+member1(R, X, P) :- R = rotor1, rotor1(NL), member(NL, X, P).
+member1(R, X, P) :- R = rotor2, rotor2(NL), member(NL, X, P).
+
